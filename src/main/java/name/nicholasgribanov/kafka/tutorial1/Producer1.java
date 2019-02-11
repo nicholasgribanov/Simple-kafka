fdae7b5b0f6e905db41de2a6bf8dc6747f44dadc
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2019-02-11 15:12:01 +0300

    Send simple message from kafka-producer based on Java to bootstrap local server

--- src/main/java/name/nicholasgribanov/kafka/tutorial1/Producer1.java
+++ src/main/java/name/nicholasgribanov/kafka/tutorial1/Producer1.java
@@ -1,7 +1,28 @@
 package name.nicholasgribanov.kafka.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class Producer1 {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        String bootstrapServer = "127.0.0.1:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "Hello, World!");
+
+        producer.send(record);
+
+        producer.close();
+
     }
 }
